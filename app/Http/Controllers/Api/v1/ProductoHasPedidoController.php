dbacc386a1a8a04181e4b474c3b20ac2760b8a54
AdrizProg <157852821+AdrizProg@users.noreply.github.com>
2025-02-11 21:22:52 +0000

    Subiendo los controladores

---
+++ app/Http/Controllers/Api/v1/ProductoHasPedidoController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Models\ProductoHasPedido;
+use Illuminate\Http\Request;
+
+class ProductoHasPedidoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ProductoHasPedido $productoHasPedido)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, ProductoHasPedido $productoHasPedido)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ProductoHasPedido $productoHasPedido)
+    {
+        //
+    }
+}
