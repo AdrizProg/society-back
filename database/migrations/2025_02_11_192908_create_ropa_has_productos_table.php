8613ce650665147e308522eea18b4777cd474da1
AdrizProg <157852821+AdrizProg@users.noreply.github.com>
2025-02-11 21:56:33 +0000

    Arreglando los seeders, falta

--- database/migrations/2025_02_11_192908_create_ropa_has_productos_table.php
+++ database/migrations/2025_02_11_192908_create_ropa_has_productos_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
         Schema::create('ropa_has_productos', function (Blueprint $table) {
             $table->id();
             $table->foreignId('producto_id')->constrained();
-            $table->foreignId('ropaTipoProducto_id')->constrained('ropa_tipo_products');
             $table->integer('cantidad');
             $table->foreignId('ropa_tipo_producto_id');
             $table->timestamps();
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Database\Factories\CategoriaHasProductoFactory;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,6 +19,20 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
+        ]);
+
+        $this->call([
+            AsociacionSeeder::class,
+            UserHasAsociacionSeeder::class,
+            RopaTipoProductoSeeder::class,
+            ProductoSeeder::class,
+            RopaHasProductoSeeder::class,
+            ImagenSeeder::class,
+            CategoriaSeeder::class,
+            CategoriaHasProductoSeeder::class,
+            PedidoSeeder::class,
+            ProductoHasPedidoSeeder::class,
+            ComentarioSeeder::class
         ]);
     }
 }
