3605cc5b8c5278d31fc5ce6540aaba4194978e11
Alexloven <reyvaldiviaalejandro@gmail.com>
2025-02-11 12:06:17 +0100

    seguimos probando el error de cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://adrian.informaticamajada.es'],
+    'allowed_origins' => ['https://society-front.vercel.app/'],
 
     'allowed_origins_patterns' => [],
 
