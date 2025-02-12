99a4801b7005d16ba43957390a0a68789d3eceeb
Alexloven <reyvaldiviaalejandro@gmail.com>
2025-02-13 00:30:39 +0100

    seguimos mejorando el orion hasta que funcione

--- app/Http/Controllers/Api/v1/CategoriaController.php
+++ app/Http/Controllers/Api/v1/CategoriaController.php
@@ -4,19 +4,15 @@ namespace App\Http\Controllers\Api\v1;
 
 use Orion\Http\Controllers\Controller;
 use App\Models\Categoria;
-use Illuminate\Http\Request;
 use Orion\Concerns\DisableAuthorization;
 use Orion\Concerns\DisablePagination;
 
 class CategoriaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     use DisablePagination;
     use DisableAuthorization;
+
     protected $model = Categoria::class;
 
-    protected $relations = ['productos'];
-
+    protected $relations = ['productos']; // Relación correcta
 }
