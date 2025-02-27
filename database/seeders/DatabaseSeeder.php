6fe4bb8d967685f5df11c728f47959130164b3eb
Alexloven <reyvaldiviaalejandro@gmail.com>
2025-02-27 17:22:48 +0100

    creamos el user admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,9 @@ class DatabaseSeeder extends Seeder
     {
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Admin',
+            'email' => 'admin@example.com',
+            'admin' => true,
         ]);
 
         Asociacion::query()->delete();
